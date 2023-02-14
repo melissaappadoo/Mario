5e84b3ad78e792ed90decc09afd4b931f7583467
Melissa Appadoo <114985119+melissaappadoo@users.noreply.github.com>
2023-02-14 10:57:13 +0000

    Setup

---
+++ BaseMarioProject-main1/BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -0,0 +1,49 @@
+//Be grateful for humble beginnings, because the next level will always require so much more of you
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+#include "constants.h"
+
+#include <iostream>
+using namespace std;
+
+//Globals
+SDL_Window* g_window = nullptr;
+
+//Function prototypes
+bool InitSDL();
+void CloseSDL();
+
+int main(int argc, char* args[])
+{
+	return 0;
+}
+
+bool InitSDL() 
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+}
+
+void CloseSDL()
+{
+
+}
